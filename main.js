85780bc4469a528e88aa56b17013d76734a4985b
Aram Mine <aram.mine@gaiax.com>
2020-06-02 13:36:29 +0900

    promized

--- main.js
+++ main.js
@@ -3,34 +3,46 @@ const driver = require('bigchaindb-driver')
 // BigchainDB server instance (e.g. https://example.com/api/v1/)
 const API_PATH = 'http://localhost:9984/api/v1/'
 
-// Create a new keypair.
-const alice = new driver.Ed25519Keypair()
-
-// Construct a transaction payload
-const tx = driver.Transaction.makeCreateTransaction(
-    // Define the asset to store, in this example it is the current temperature
-    // (in Celsius) for the city of Berlin.
-    { city: 'Kawasaki, JP', temperature: 21, datetime: new Date().toString() },
-
-    // Metadata contains information about the transaction itself
-    // (can be `null` if not needed)
-    { what: 'My first BigchainDB transaction' },
-
-    // A transaction needs an output
-    [
-        driver.Transaction.makeOutput(
-            driver.Transaction.makeEd25519Condition(alice.publicKey)
-        ),
-    ],
-    alice.publicKey
-)
-
-// Sign the transaction with private keys
-const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
-
 // Send the transaction off to BigchainDB
 const conn = new driver.Connection(API_PATH)
+main()
 
-conn.postTransactionCommit(txSigned).then((retrievedTx) =>
-    console.log('Transaction', retrievedTx.id, 'successfully posted.')
-)
+async function main() {
+    // Create a new keypair.
+    let alice = new driver.Ed25519Keypair()
+
+    let data = {}
+    data.asset = {
+        city: 'Kawasaki, JP',
+        temperature: 21,
+        datetime: new Date().toString(),
+    }
+    data.meta = { what: 'My first BigchainDB transaction' }
+
+    let result
+    result = await writeToChain(conn, data, alice)
+    console.log(result)
+}
+
+async function writeToChain(conn, data, keypair) {
+    // Construct a transaction payload
+    const tx = driver.Transaction.makeCreateTransaction(
+        // Define the asset to store
+        data.asset,
+        // Metadata contains information about the transaction itself
+        // (can be `null` if not needed)
+        data.meta,
+        // A transaction needs an output
+        [
+            driver.Transaction.makeOutput(
+                driver.Transaction.makeEd25519Condition(keypair.publicKey)
+            ),
+        ],
+        keypair.publicKey
+    )
+
+    // Sign the transaction with private keys
+    const txSigned = driver.Transaction.signTransaction(tx, keypair.privateKey)
+
+    return await conn.postTransactionCommit(txSigned)
+}
